430dd23d197a4dd189790e5299667ab5ea64b066
Adam Urbina <adamurbina@gmail.com>
2019-01-22 22:08:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function lowerCaseDrivers(collection) {
   return collection.map(item => item.toLowerCase());
 }
 
-function nameToAttributes() {
-
+function nameToAttributes(collection) {
+  return collection.map(s => firstName: s.split[0], lastName: s.split[1]);
 }
 
 function attributesToPhrase() {
